9434bafc708351e5938b929e2883d5a0c3527313
tryhardov <tryhardov123@gmail.com>
2026-06-26 00:06:11 +0300

    initotkat

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,4 @@ from .views import block_list
 urlpatterns = [
     path('', block_list, name='block_list'),
     path('category/<slug:category_slug>/', block_list, name='block_list_by_category'),
-    path('user/<str:username>/', block_list, name='block_list_by_username')
 ]
--- main/views.py
+++ main/views.py
@@ -2,24 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Profile, Block
 from django.contrib.auth.models import User
 
-def block_list(request, category_slug=None, username=None):
+def block_list(request, category_slug=None):
     categories = Category.objects.all()
     blocks = Block.objects.filter(is_public=True)
     category = None
-    user = None
 
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         blocks = blocks.filter(category=category)
     
-    if username:
-        user = get_object_or_404(User, username=username)
-        blocks = blocks.filter(owner=user)
-
     return render(request, 'main/main.html', {'categories' : categories,
                                               'blocks' : blocks,
-                                              'category' : category,
-                                              'user' : user})
+                                              'category' : category})
+
 
     
 
