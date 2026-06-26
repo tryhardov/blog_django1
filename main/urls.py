65a48585f13a393c47780180d6085b62be520aa0
tryhardov <tryhardov123@gmail.com>
2026-06-26 15:22:00 +0300

    init2

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import block_list
+from .views import block_list, block_detail, user_page, user_search
 
 urlpatterns = [
     path('', block_list, name='block_list'),
     path('category/<slug:category_slug>/', block_list, name='block_list_by_category'),
+    path('<slug:slug>/detail/', block_detail, name='block_detail'),
+    path('user/search/', user_search, name='user_search'),
+    path('user/<str:username>/', user_page, name='user_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Profile, Block
 from django.contrib.auth.models import User
 
@@ -15,6 +15,28 @@ def block_list(request, category_slug=None):
                                               'blocks' : blocks,
                                               'category' : category})
 
+def block_detail(request, slug):
+    block = get_object_or_404(Block, slug=slug)
+
+    return render(request, 'main/detail.html', {'post': block})
+
+def user_page(request, username):
+    user = get_object_or_404(User, username__iexact=username)
+    profile = get_object_or_404(Profile, user=user)
+    blocks = Block.objects.filter(owner=user)
+
+    return render(request, 'main/user.html', {'profile' : profile,
+                                              'blocks' : blocks,
+                                              'user' : user})
+
+def user_search(request):
+    query = request.GET.get('q')
+
+    if query:
+        user = get_object_or_404(User, username__iexact=query)
+        return redirect('user_page', username=user.username)
+    
+    return redirect('block_list')
 
     
 
